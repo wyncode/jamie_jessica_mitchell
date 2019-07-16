7f26549b95b4a1c35809a7a766efc0646d36d38b
Jamie Katz <jamiekatz11@gmail.com>
2019-07-16 16:20:46 -0400

    Added images folder to the public directory

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,7 +4,7 @@ import "../Components/CSS/homepage.css";
 const Home = () => (
     <>
       <div id="logo">
-        <img src="/client/pages/Picture1.png" alt="Logo"/>
+        <img src="/images/Picture1.png" alt="Logo"/>
       </div>
       <div>
         <h3>Looking to learn more about nutrition?</h3>
