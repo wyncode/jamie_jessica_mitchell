cdb04b7b27e0203dec487d1c381751a529ddf7dc
Jamie Katz <jamiekatz11@gmail.com>
2019-07-16 16:02:47 -0400

    fixed indentation

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,13 +3,13 @@ import "../Components/CSS/homepage.css";
 
 const Home = () => (
     <>
-        <div id="logo">
-          <img src="/client/pages/Picture1.png" alt="Logo"/>
-        </div>
-        <div>
-          <h3>Looking to learn more about nutrition?</h3>
-          <button>Get your personalized meal plan here!</button>
-        </div>
+      <div id="logo">
+        <img src="/client/pages/Picture1.png" alt="Logo"/>
+      </div>
+      <div>
+        <h3>Looking to learn more about nutrition?</h3>
+        <button>Get your personalized meal plan here!</button>
+      </div>
     </>
     )
 
