22db539a63ba2988350438c5236bbb1c608c56a0
mposk <mposk@rbi.com>
2019-07-16 15:54:12 -0400

    still cleaning the createplan page, and capitalized file name

--- client/src/pages/createplan.js
+++ client/src/pages/createplan.js
@@ -1,4 +1,5 @@
 import React from "react";
+import unirest from "unirest";
 
 //CreatePlan Form Component
 //Diet types, foods you don't like, health level, meals per day
@@ -6,42 +7,28 @@ import React from "react";
 
 class CreatePlan extends React.Component {
   state = {
-    selectValue: "none",
-    dontLikeValue: "",
-    healthValue: "",
-    mealsValue: ""
+    recipes: [],
+    selectDiet: "none",
+    dontLike: "",
+    calorieScale: "2500"
   };
 
   //Form Submittion Functioon
   handleSubmit = event => {
     event.preventDefault();
-    // unirest
-    //   .get(`/food`)
-    //   .then(response => this.setState({ movies: response.data }));
+    unirest
+      .get(`/food`)
+      .then(response => this.setState({ recipes: response.data }));
   };
 
-  //OnChange Function for Select Input
-  handleSelectChange = event => {
-    this.setState({ selectValue: event.target.value });
-  };
-
-  //OnChange Function for Things You Don't Like Input Values
-  handleDontLikeChange = event => {
-    this.setState({ dontLikeValue: event.target.value });
-  };
-
-  //OnChange Function for Health Scale
-  handleHealth = event => {
-    this.setState({ healthValue: event.target.value });
-  };
-
-  //OnChange Function for Meals Per Day
-  handleMealsChange = event => {
-    this.setState({ mealsValue: event.target.value });
+  // onChange function for input options
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
   };
 
   //Render Function
   render() {
+    const { selectDiet, dontLike, calorieScale } = this.state;
     return (
       <>
         <h1>Form</h1>
@@ -51,53 +38,48 @@ class CreatePlan extends React.Component {
             <label htmlFor="selectDiet">Diet Options: </label>
             <select
               id="selectDiet"
-              value={this.state.selectValue}
-              onChange={this.handleSelectChange}
+              name="selectDiet"
+              value={selectDiet}
+              onChange={this.handleChange}
             >
               <option value="choose">Choose One...</option>
               <option value="vegan">Vegan</option>
-              <option value="vegetarian">Vegetarian</option>
-              <option value="gluten-free">Gluten-Free</option>
+              <option value="vegetarian">Vegetarian (Lacto and Ovo)</option>
               <option value="paleo">Paleo</option>
               <option value="ketogenic">Ketogenic</option>
-              <option value="none">None</option>
+              <option value="primal">Primal</option>
             </select>
           </div>
           {/* Things You Don't Like */}
           <div className="dontLikeSelector">
-            <label htmlFor="dontLike">Foods you don't like: </label>
+            <label htmlFor="dontLike">Dislikes: </label>
             <input
               id="dontLike"
+              name="dontLike"
               type="text"
               placeholder="eg. squash, cantaloupe"
-              value={this.state.dontLikeValue}
-              onChange={this.handleDontLikeChange}
+              value={dontLike}
+              onChange={this.handleChange}
             />
           </div>
-          {/* Health Scale */}
-          <div className="healthScale">
-            <label htmlFor="healthRange">Healthiness Range: </label>
+          {/* Calorie Scale */}
+          <div className="calorieScale">
+            <label htmlFor="calorieRange">Maximum Calories: </label>
             <input
+              id="calorieRange"
+              name="calorieScale"
               type="range"
-              min="0"
-              max="100"
-              value={this.state.healthValue}
-              onChange={this.handleHealth}
+              min="1800"
+              max="5000"
+              value={calorieScale}
+              onChange={this.handleChange}
             />
+            {calorieScale} calories per day
           </div>
-          {/* Meals Per Day */}
-          <div className="mealsPerDaySelector">
-            <label htmlFor="mealsPerDay">Meals Per Day: </label>
-            <input
-              id="mealsPerDay"
-              type="number"
-              min="3"
-              max="5"
-              step="1"
-              value={this.state.Value}
-              onChange={this.handleMealsChange}
-            />
-          </div>
+          {/* Alergy Selector
+          <div className="alergySelector">
+            <label htmlFor="cuisine">Allergies: </label>
+          </div> */}
           <div className="formbutton">
             <button>Submit</button>
           </div>
