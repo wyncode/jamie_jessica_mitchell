d2461b498de5660f2a2f4f4b2faece79837a92de
Jamie Katz <jamiekatz11@gmail.com>
2019-07-15 22:48:13 -0400

    added home page content and css -- must understand issue with logo image

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./Components/CSS/App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePlan from "./pages/CreatePlan";
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import "../Components/CSS/homepage.css";
 
-const Home = () => {
-  return <h1>Hello</h1>;
-};
+const Home = () => (
+    <>
+      <body>
+        <div id="logo">
+          <img src="/client/pages/Picture1.png" alt="Logo"/>
+        </div>
+        <div>
+          <h3>Looking to learn more about nutrition?</h3>
+          <button>Get your personalized meal plan here!</button>
+        </div>
+      </body>
+    </>
+    )
 
-export default Home;
+export default Home
