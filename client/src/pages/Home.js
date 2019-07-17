06c6ea36e7075381dd04aae1b6eacd282b82186e
Jamie Katz <jamiekatz11@gmail.com>
2019-07-16 22:10:16 -0400

    updating home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,13 +4,13 @@ import "../Components/CSS/homepage.css";
 const Home = () => (
     
       <div id="wrapper">
-      <div id="logo">
-        <img src="/images/Picture1.png" alt="Logo"/>
-        <h3>Looking to learn more about nutrition?</h3>
-        <div id="button">
-        <button>Get your personalized meal plan here!</button>
+        <div id="logo">
+          <img src="/images/Picture1.png" alt="Logo"/>
+          <h3>Looking to learn more about nutrition?</h3>
+          <div id="button">
+            <button>Get your personalized meal plan here!</button>
+          </div>
         </div>
-      </div>
       </div>
     )
 
