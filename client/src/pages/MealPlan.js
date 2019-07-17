6eb4a77f351cb9b91e28b1032baef8f1376677d1
Jamie Katz <jamiekatz11@gmail.com>
2019-07-17 15:37:34 -0400

    mealplan.js structure

--- client/src/pages/MealPlan.js
+++ client/src/pages/MealPlan.js
@@ -9,26 +9,41 @@ class MealPlan extends React.Component {
   render() {
     return (
       <div id="Meal Plan">
-        <h1 id="title">Meal Plan</h1>
-        <div className="card-group">
+        <h1 id="title">Meals for the Week</h1>
+        <div className="card-group d-flex flex-wrap flex-md-row flex-column"
+        >
           
           {
             this.props.recipes.map(recipe => {
               console.log(recipe);
               return (
-                <div className="card">
-                  <img src={recipe.image} className="card-img-top" alt="..."/>
-                  <div className="card-body">
-                    <h5 className="card-title">{recipe.title}</h5>
-                    <p className="card-text">{recipe.instructions}</p>
-                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                  <div className="card">
+                    <a 
+                      href={recipe.sourceUrl}
+                      target="_blank"
+                    >
+                        <img
+                          src={recipe.image} 
+                          className="card-img-top" 
+                          alt="recipe"
+                        /> 
+                    </a>
+                    <div className="card-body">
+                      <h5 className="card-title">{recipe.title}</h5>
+                      <p className="card-text"><small className="text-muted">Ready in {recipe.readyInMinutes} minutes</small></p>
+                    </div>
+                  
                   </div>
-                </div>
+                
               )
             })
           }
         </div>
+        <div id="blurb">
+          We've selected 7 meals that fit your individual diet and needs.Bon Apetite! 
+        </div>
       </div>
+     
     );
   }
 }
