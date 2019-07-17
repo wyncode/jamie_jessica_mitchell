7c5704a6f7e6fc0ed5c3be6a3ecd1faae3766dff
Jamie Katz <jamiekatz11@gmail.com>
2019-07-16 20:15:39 -0400

    style changes to homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,12 +3,14 @@ import "../Components/CSS/homepage.css";
 
 const Home = () => (
     <>
+      <div id="wrapper">
       <div id="logo">
         <img src="/images/Picture1.png" alt="Logo"/>
-      </div>
-      <div>
         <h3>Looking to learn more about nutrition?</h3>
+        <div id="button">
         <button>Get your personalized meal plan here!</button>
+        </div>
+      </div>
       </div>
     </>
     )
